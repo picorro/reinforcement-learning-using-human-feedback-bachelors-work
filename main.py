2f8bed9b2e0137938ff517ce89bf9a7e72c816ea
dalius.budrys <dalius.budrys@unity3d.com>
2023-03-28 00:21:25 +0300

    Random

--- main.py
+++ main.py
@@ -8,20 +8,21 @@ from video import *
 
 def main():
 
-   # result = HumanFeedback.requestHumanFeedbackFromVideos("./videos/test.mp4", "./videos/test.mp4", 1200, 500)
-   # print(result)
-    # do something if the user finds the videos incomparable
    env = gym.make("CartPole-v1")
    model = A2C("MlpPolicy", env)
-   model.learn(total_timesteps=int(1000))
+   model.learn(total_timesteps=int(10000), progress_bar=True)
 
-   model.save("a2c_lunar")
+   # model.save("a2c_lunar")
 
-   del model
-   model = A2C.load("a2c_lunar")
+   # del model
+   # model = A2C.load("a2c_lunar")
 
    render_env = gym.make("CartPole-v1", render_mode="rgb_array")
    VideoMaker.getVideoFromEnvAndModel(render_env, model, 30, 1000, 600, 400, "test.mp4")
+
+   HumanFeedback.playVideo("./videos/test.mp4", 600, 400)
+
+
 
    env.close()
    render_env.close()
