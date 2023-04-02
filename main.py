a835679d8be6803264147f1e1e5b27c7c39a19aa
dalius.budrys <dalius.budrys@unity3d.com>
2023-04-02 13:02:17 +0300

    Add template dataset generation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import d3rlpy
-
 from d3rlpy.datasets import get_cartpole # CartPole-v0 dataset
 from d3rlpy.metrics.scorer import evaluate_on_environment
 from d3rlpy.metrics.scorer import td_error_scorer
@@ -14,7 +13,9 @@ start_time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 
 parser = argparse.ArgumentParser(description="Parsing module.")
 parser.add_argument("-a", "--algorithm_name", type=str, required=True, help="Algorithm: --dqn")
-parser.add_argument("-l", "--load", type=str, help="Model file location")
+parser.add_argument("-m", "--mode", type=str, help="Program mode: --demo --dataset")
+parser.add_argument("-s", "--steps", type=int, help="Step count for any operations the program might do related to model training or dataset generation")
+parser.add_argument("-tm", "--trained_model", type=str, help="Trained model file name yyyy-mm-dd-HH-MM-SS")
 args = parser.parse_args()
 
 
@@ -22,36 +23,51 @@ dataset, env = get_cartpole()
 
 
 algorithm_name = args.algorithm_name
-algo = None
+algorithm = None
 
-if args.load != None:
-    if algorithm_name == 'dqn':
-        algo = d3rlpy.algos.DQN()
-        algo.build_with_dataset(dataset)
-        algo.load_model(f'./trained_models/{algorithm_name}/{args.load}.pt')
+if args.mode == "demo":
+    if algorithm_name == "dqn":
+        algorithm = d3rlpy.algos.DQN()
+        algorithm.build_with_dataset(dataset)
+        algorithm.load_model(f"./trained_models/{algorithm_name}/{args.load}.pt")
         
         evaluate_scorer = evaluate_on_environment(env, render=True)
-        rewards = evaluate_scorer(algo)
+        rewards = evaluate_scorer(algorithm)
         sys.exit()
+elif args.mode == "dataset":
+    if algorithm_name == "dqn":
+        algorithm = d3rlpy.algos.DQN()
+    algorithm.build_with_env(env)
+    algorithm.load_model(f"./trained_models/{algorithm_name}/{args.trained_model}.pt")
+    buffer = d3rlpy.online.buffers.ReplayBuffer(maxlen=args.steps, env=env)
+    algorithm.collect(env, buffer, n_steps=args.steps)
+    dataset = buffer.to_mdp_dataset()
+
+    pathExists = os.path.exists(f'./datasets/{algorithm_name}')
+    if not pathExists:
+        os.makedirs(f'datasets/{algorithm_name}')
+    dataset.dump(f"./datasets/{algorithm_name}/{start_time}.h5")
+    sys.exit()
+
 
     
 
 train_episodes, test_episodes = train_test_split(dataset, test_size=0.2)
 
-algo = d3rlpy.algos.DQN()
-algo.build_with_dataset(dataset)
-td_error = td_error_scorer(algo, test_episodes)
+algorithm = d3rlpy.algos.DQN()
+algorithm.build_with_dataset(dataset)
+td_error = td_error_scorer(algorithm, test_episodes)
 
 # train offline
-algo.fit(dataset, n_steps=1000, n_steps_per_epoch=1000)
+algorithm.fit(dataset, n_steps=args.steps, n_steps_per_epoch=1000)
 
 evaluate_scorer = evaluate_on_environment(env, render=True)
-rewards = evaluate_scorer(algo)
+rewards = evaluate_scorer(algorithm)
 
 # Save trained model
 pathExists = os.path.exists(f'./trained_models/{algorithm_name}')
 if not pathExists:
     os.makedirs(f'trained_models/{algorithm_name}')
 
-algo.save_model(f'./trained_models/{algorithm_name}/{start_time}.pt')
+algorithm.save_model(f'./trained_models/{algorithm_name}/{start_time}.pt')
 
